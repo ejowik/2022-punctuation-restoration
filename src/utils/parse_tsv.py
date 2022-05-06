7a29c325836bd95a86a12acebbfd06e10eb80f5d
ejowik <elzbietajowik@gmail.com>
2022-05-06 21:02:06 +0200

    solution init commit

---
+++ src/utils/parse_tsv.py
@@ -0,0 +1,103 @@
+import sys
+
+
+def __read_times_data(path):
+    data = []
+    for line in open(path, encoding="utf-8"):
+        if line[-1] == "\n":
+            line = line[:-1]
+        if line == "</s>":
+            continue
+        times, text = line.split(" ")
+        start, end = times[1:-1].split(",")
+        start = int(start)
+        end = int(end)
+
+        data.append((start, end, text))
+    return data
+
+
+def __times_after_tokens(path):
+    data = __read_times_data(path)
+    result = []
+    for i in range(len(data)):
+        start, end, text = data[i]
+        label = text[-1] if text[-1] in ",!?.:;-" else "B"
+
+        try:
+            br = data[i + 1][0] - end
+        except IndexError:
+            br = 0
+        result.append((text, br))
+    return result
+
+
+def __match_times(times, expected):
+    matched = []
+
+    times_text = ""
+    times_indexes = {}
+    for token, time in times:
+        times_indexes[len(times_text)] = time
+        times_text = token.lower()
+
+    index = 0
+    for token in expected:
+        # print(token)
+        found_index = times_text.find(token.lower(), index)
+        if found_index >= 0:
+            if found_index in times_indexes:
+                matched.append(times_indexes[found_index])
+                index = found_index
+            else:
+                matched.append(-1)
+        else:
+            matched.append(-1)
+    return matched
+
+
+def parse_tsv(
+    in_path, expected_path, save_path, clntmstmp_dir=None, files_to_ignore=[]
+):
+    out = open(save_path, "w", encoding="utf-8")
+
+    for in_line, expected in zip(
+        open(in_path, encoding="utf-8"), open(expected_path, encoding="utf-8")
+    ):
+        if in_line[-1] == "\n":
+            in_line = in_line[:-1]
+        name, text = in_line.split("\t")
+
+        if name in files_to_ignore:
+            continue
+
+        if expected[-1] == "\n":
+            expected = expected[:-1]
+
+        assert len(text.split(" ")) == len(expected.split(" "))
+
+        if clntmstmp_dir:
+            times = __times_after_tokens(clntmstmp_dir + f"/{name}.clntmstmp")
+            matched = __match_times(times, expected.split(" "))
+
+        for i, (in_token, expected_token) in enumerate(
+            zip(text.split(" "), expected.split(" "))
+        ):
+            expected_token = expected_token.lower()
+            if in_token == expected_token:
+                label = "B"
+            else:
+                if in_token == expected_token[:-1]:
+                    label = expected_token[-1]
+                # elif in_token == expected_token[:-3] and expected_token[-3:] == "...":
+                #     label = expected_token[-3:]
+                elif in_token == expected_token[:len(in_token)]:
+                    label = expected_token[len(in_token)]
+                else:
+                    print("ERROR", in_token, expected_token, file=sys.stderr)
+
+            if clntmstmp_dir:
+                out.write(f"{in_token}\t{label}\t{matched[i]}\n")
+            else:
+                out.write(f"{in_token}\t{label}\n")
+        out.write("\n")
